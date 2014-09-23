b87f163ba51e81abde121d2699529530bd42faaf
Cameron Lakenen <lakenen@box.com>
2014-09-23 09:08:44 -0700

    Fix attiny subdir issue

--- update-deps.sh
+++ update-deps.sh
@@ -11,17 +11,18 @@ update_repo() {
 
 # HARDWARE
 echo "Installing hardware..."
-mkdir -p hardware
-cd hardware
+mkdir -p hardware && cd hardware
 # https://github.com/damellis/attiny
 update_repo "attiny" "damellis/attiny"
+# The attiny repo contains the stuff in a second attiny subdir
+# TODO: figure out a better way to get the correct attiny subdir
+[ -f attiny85 ] && ln -s attiny/attiny attiny85
 
 cd ..
 
 # LIBRARIES
 echo "Installing libraries..."
-mkdir -p libraries
-cd libraries
+mkdir -p libraries && cd libraries
 # https://github.com/milesburton/Arduino-Temperature-Control-Library
 update_repo "DallasTemperature" "milesburton/Arduino-Temperature-Control-Library"
 
